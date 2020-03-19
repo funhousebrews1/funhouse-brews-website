56e7845ac7c806a05ab1b63b393f76d0af0a53fe
funhousebrews1 <60830707+funhousebrews1@users.noreply.github.com>
2020-03-18 18:20:42 -0700

    a bad change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
-    <header id="header" className="alt">
+    <header id="header" className="alt" /><??>
         <span className="logo"><img src={logo} alt="" /></span>
         <h2>Portland, Oregon</h2>
     </header>
