f5c490cb8ce171cd59c1f0e81fa77ed8eb24e3a3
funhousebrews1 <60830707+funhousebrews1@users.noreply.github.com>
2020-05-15 19:39:06 -0700

    Update index.js
    may 21 nehalim

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,7 +146,7 @@ const Index = ({ data }) => {
         <section id="second" className="main special">
           <header className="major">
             <h2>Growler Fill Thursdays!</h2>
-            <h3>Special Guest Nehalem River Ranch Thursday May7 from 4-8 p.m. 
+            <h3>Special Guest Nehalem River Ranch Thursday May 21 from 4-8 p.m. 
 Order in advance here <a href="https://www.nehalemriverranch.com/products" target="blank">at 
 Nehalem River Ranch</a><img src="https://images.squarespace-cdn.com/content/5cfa9c7aeb4639000180098d/1560181363290-V5374X44EI6L5WJOVU7K/logo.png"></img></h3>
 <p className="line-breaks">
