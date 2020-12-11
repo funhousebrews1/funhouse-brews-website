faf2d9711e5971b742a3e48ed8ca96299343d680
funhousebrews1 <60830707+funhousebrews1@users.noreply.github.com>
2020-12-10 19:59:01 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,8 +73,8 @@ const Index = ({ data }) => {
                <p>Check  out our partners: <a href="https://www.nehalemriverranch.com/products" target="blank">at
                 Nehalem River Ranch</a><img src="https://images.squarespace-cdn.com/content/5cfa9c7aeb4639000180098d/1560181363290-V5374X44EI6L5WJOVU7K/logo.png"></img></p>
                 <p className="line-breaks"> </p>
-                  <h2>Terra Farma</h2><a href="https://terrafarmers.net" target="blank">
-                  <img src="http://terrafarmers.net/wp-content/uploads/2014/07/cropped-terrafarmabanner1.png"></img>
+                  <h2>Terra Farma</h2> <a href="https://terrafarmers.net" target="blank">
+                  <img src="http://terrafarmers.net/wp-content/uploads/2014/07/cropped-terrafarmabanner1.png"></img> </a>
                   
                               
    <p>
